77ddf7d5c06bdfcf180654d307a5be2d8d45e24e
Florencia <florenciabelenfigueroa05@gmail.com>
2025-06-25 23:23:50 -0300

    Add files via upload

---
+++ demo_menu/ScripsMenu/ScriptMenu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+#Funcion boton nuevo juego:
+func _on_bnuevo_juego_pressed() -> void:
+
+
+#Funcion boton continuar partida:
+func _on_bcontinuar_partida_pressed() -> void:
+
+
+#Funcion boton exit:
+func _on_bexit_pressed() -> void:
+	get_tree().quit()
+
+
+#Funcion boton settings
+func _on_bsettings_pressed() -> void:
