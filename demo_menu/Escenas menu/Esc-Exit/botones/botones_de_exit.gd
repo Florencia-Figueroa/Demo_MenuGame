190f7d00e1c8263d404685c39e0b40fe987d04d4
hyunanoona <amelie.paz@estudiantes.unahur.edu.ar>
2025-07-02 22:12:39 -0300

    avances en los settings

--- demo_menu/Escenas menu/Esc-Exit/botones/botones_de_exit.gd
+++ demo_menu/Escenas menu/Esc-Exit/botones/botones_de_exit.gd
@@ -3,7 +3,7 @@ extends Control
 
 
 func _on_volver_pressed() -> void:
-	get_tree().change_scene_to_file("res://Escenas menu/EscenaMenu/escena menu.tscn")
+	get_tree().change_scene_to_file("res://Escenas menu/Esc-MenuInicio/escena menu.tscn")
 
 func _on_salir_pressed() -> void:
 	get_tree().quit()
--- demo_menu/Escenas menu/Esc-MenuInicio/botones/menu.gd
+++ demo_menu/Escenas menu/Esc-MenuInicio/botones/menu.gd
@@ -1,17 +1,16 @@
 extends Control
 #Funcion boton nuevo juego:
 func _on_bnuevo_juego_pressed() -> void:
-	get_tree().change_scene_to_file("res://Escenas Menu/EscenaNuevoJuego/escena nuevo juego.tscn")
+	get_tree().change_scene_to_file("res://Escenas menu/Esc-NuevoJuego/escena nuevo juego.tscn")
 
 #Funcion boton continuar partida:
 func _on_bcontinuar_partida_pressed() -> void:
-	get_tree().change_scene_to_file("res://Escenas Menu/EscenaContinuarJuego/escena continuar juego.tscn")
+	get_tree().change_scene_to_file("res://Escenas menu/Esc-ContinuarJuego/escena continuar juego.tscn")
 
 #Funcion boton exit:
 func _on_bexit_pressed() -> void:
-	get_tree().change_scene_to_file("res://Escenas Menu/EscenaExit/escena exit.tscn")
-
+	get_tree().change_scene_to_file("res://Escenas menu/Esc-Exit/escena exit.tscn")
 
 #Funcion boton settings
 func _on_bsettings_pressed() -> void:
-	get_tree().change_scene_to_file("res://Escenas Menu/EscenasSettings/escena settings.tscn")
+	get_tree().change_scene_to_file("res://Escenas menu/Esc-Settings/escena setting.tscn")
