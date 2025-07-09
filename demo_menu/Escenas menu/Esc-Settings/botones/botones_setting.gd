0499137c04a139915acc8eb9e6f42248b89d15a7
Florencia <florenciabelenfigueroa05@gmail.com>
2025-07-09 00:01:26 -0300

    Arreglo sonido

--- demo_menu/Escenas menu/Esc-Settings/botones/botones_setting.gd
+++ demo_menu/Escenas menu/Esc-Settings/botones/botones_setting.gd
@@ -3,3 +3,14 @@ extends Control
 #Funcion del boton volver escena opciones:
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://Escenas menu/Esc-MenuInicio/escena menu.tscn")
+	
+
+@onready var volumen_slider = $Panel/Panel/HSlider
+
+func _ready():
+	volumen_slider.value = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Sonido General"))
+  
+
+func _on_h_slider_value_changed(value: float):
+	var bus_index = AudioServer.get_bus_index("Sonido General")
+	AudioServer.set_bus_volume_db(bus_index, value)
--- demo_menu/Sonido/audio.gd
+++ demo_menu/Sonido/audio.gd
@@ -1,7 +1,6 @@
 extends AudioStreamPlayer
 
-var musica = preload("res://Sonido/End of Line (From TRON_ LegacyScore).mp3")
+
 
 func _ready():
-	Audio.stream = musica 
 	Audio.play()
